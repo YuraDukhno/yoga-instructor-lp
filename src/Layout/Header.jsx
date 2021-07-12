ef351b2cadd326aeec0c59d9e8dcf493b9ef3fe1
Yura Dukhno <yura.dukhno@gmail.com>
2021-07-12 14:05:29 +0300

    added styles to header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -7,14 +7,28 @@ export default function Header() {
       <div className="container header__container">
         <div className="header__content">
           <h1 className="header__title">I'm</h1>
-          <br />
+
           <h1 className="header__title header__title-yalow"> lisa zak.</h1>
-          <div className="header__bottom-line"></div>
+          <div className="header__bottom-line">
+            <div className="line"></div>
+          </div>
           <div className="header__subtitle">Yoga Instructor</div>
           <ul className="icon__list">
-            <li className="icon">facebook</li>
-            <li className="icon">instagram</li>
-            <li className="icon">twitter</li>
+            <li className="icon uc_icons_link">
+              <a href="http://www.facebook.com" className="uc_icon">
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className="icon uc_icons_link">
+              <a href="http://www.facebook.com" className="uc_icon">
+                <i className="fa fa-instagram"></i>
+              </a>
+            </li>
+            <li className="icon uc_icons_link">
+              <a href="http://www.facebook.com" className="uc_icon">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
